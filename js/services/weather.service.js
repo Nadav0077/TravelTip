4f98a1f2c75e9b4cc036f6b088a565eb1c417cde
tamirmatz <tamirmatz@gmail.com>
2021-04-27 16:23:29 +0300

    ascascacs

--- js/services/weather.service.js
+++ js/services/weather.service.js
@@ -1,6 +1,8 @@
 'use strict'
+import { locService } from './loc.service.js'
 
 const W_KEY = '5cc2af20f9064b14db9e3508bda4d057'
+
 
 export const weatherService = {
     getWeather
@@ -8,6 +10,38 @@ export const weatherService = {
 
 
 function getWeather(lat = 31.952110800000003, lng = 34.906551) {
-    return axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&APPID=${W_KEY}`).then(res => { res.data.temp, res.data.weather[0].description });
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&APPID=${W_KEY}`)
+        .then(res => res.data)
+        .then(resData => {
+            return {
+                temp: resData.temp,
+                desc: resData.weather[0].description
+            }
+        })
+}
 
+//res.data.temp, res.data.weather[0].description
+
+function doWeahter(idx) {
+    if (!idx) {
+        locService.getLocs().forEach(loc => {
+            axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&APPID=${W_KEY}`)
+                .then(res => res.data)
+                .then(resData => {
+                    loc.weather = {
+                        temp: resData.temp,
+                        desc: resData.weather[0].description
+                    }
+                })
+        })
+    } else {
+        axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&APPID=${W_KEY}`)
+            .then(res => res.data)
+            .then(resData => {
+                locService.getLocs()[idx].weather = {
+                    temp: resData.temp,
+                    desc: resData.weather[0].description
+                }
+            })
+    }
 }
